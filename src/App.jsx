4ef9f9d4eb1d04d09cacca4908c50ab02e72cd98
Sofia Yanchuk <sofiia.yanchuk@student.karazin.ua>
2026-05-01 18:17:00 +0300

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Game from './pages/Game'
 import MockGame from './pages/MockGame'
 import YanchukSofiaGame from './pages/YanchukSofia'
+import KhomchenkoVitaliy from './pages/KhomchenkoVitaliy'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="game" element={<Game />} />
         <Route path="mock-game" element={<MockGame />} />
         <Route path="yanchuk-sofia" element={<YanchukSofiaGame />} />
+        <Route path="khomchenko-vitaliy" element={<KhomchenkoVitaliy />} />
       </Route>
     </Routes>
   )
