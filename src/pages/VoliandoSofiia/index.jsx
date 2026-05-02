b2ae207385397c1efb5105cdb9f387caa8293e14
Sofiya_vol <voliando.sofiia@student.karazin.ua>
2026-05-02 13:35:32 +0300

    Update index.jsx

--- src/pages/VoliandoSofiia/index.jsx
+++ src/pages/VoliandoSofiia/index.jsx
@@ -1,47 +1,26 @@
 import React from 'react';
-import styles from './Minesweeper.module.css';
-import Header from './components/Header';
-import Board from './components/Board';
-import GameMessage from './components/GameMessage';
-import DifficultySelector from './components/DifficultySelector';
-import useGameLogic from './hooks/useGameLogic';
+import VoliandoSofiiaGame from '../VoliandoSofiia';
 
-function VoliandoSofiiaGame() {
-  const {
-    gameState,
-    board,
-    handleCellClick,
-    handleRightClick,
-    handleNewGame,
-    handleDifficultyChange,
-    getCellValue
-  } = useGameLogic();
+const implementations = {
+  'voliando-sofiia': VoliandoSofiiaGame,
+};
 
-  return (
-    <div className={styles.container}>
-      <Header
-        minesCount={gameState.totalMines - gameState.flagsPlaced}
-        seconds={gameState.seconds}
-        onNewGame={handleNewGame}
-      />
-      
-      <Board
-        board={board}
-        rows={gameState.rows}
-        cols={gameState.cols}
-        onCellClick={handleCellClick}
-        onCellRightClick={handleRightClick}
-        getCellValue={getCellValue}
-      />
-      
-      <GameMessage message={gameState.message} />
-      
-      <DifficultySelector
-        currentDifficulty={gameState.currentDifficulty}
-        onDifficultyChange={handleDifficultyChange}
-      />
-    </div>
-  );
+function GameList() {
+  const path = window.location.pathname.slice(1);
+  const GameComponent = implementations[path];
+
+  if (!GameComponent) {
+    return (
+      <div className="game-list">
+        <h1>Виберіть гру:</h1>
+        <ul>
+          <li><a href="/voliando-sofiia">Voliando Sofiia - Minesweeper</a></li>
+        </ul>
+      </div>
+    );
+  }
+
+  return <GameComponent />;
 }
 
-export default VoliandoSofiiaGame;
+export default GameList;
